0798b99233d492701904e4c5c71ceee8388c89b1
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-20 16:35:06 +0430

    Refactor the code and decrease the number of states

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import Header from './components/Header'
-import React , {useState} from 'react';
+import { useState } from 'react';
 import Score from './components/Score';
 import Card from './components/Card';
 import './App.css';
 import { ImageData } from './components/Data';
 
-function App() {
+export default function App() {
 
-  var [count, setCount] = useState(0);
-  var [bestScore, setBest] = useState(0);
-  var [pic1click, setPic1] = useState(0);
-  var [pic2click, setPic2] = useState(0);
-  var [pic3click, setPic3] = useState(0);
-  var [pic4click, setPic4] = useState(0);
-  var [pic5click, setPic5] = useState(0);
-  var [pic6click, setPic6] = useState(0);
-  var [pic7click, setPic7] = useState(0);
-  var [pic8click, setPic8] = useState(0);
+  const [picClick, setPicClick] = useState([
+    0,0,0,0,0,0,0,0
+  ])
+  const [count, setCount] = useState(0);
+  const [bestScore, setBest] = useState(0);
   const [elements, setElements] = useState(
     [
-      {function:pic1, src:ImageData[0].src, caption:ImageData[0].caption },
-      {function:pic2, src:ImageData[1].src, caption:ImageData[1].caption },
-      {function:pic3, src:ImageData[2].src, caption:ImageData[2].caption },
-      {function:pic4, src:ImageData[3].src, caption:ImageData[3].caption },
-      {function:pic5, src:ImageData[4].src, caption:ImageData[4].caption },
-      {function:pic6, src:ImageData[5].src, caption:ImageData[5].caption },
-      {function:pic7, src:ImageData[6].src, caption:ImageData[6].caption },
-      {function:pic8, src:ImageData[7].src, caption:ImageData[7].caption }
+      { src:ImageData[0].src, caption:ImageData[0].caption },
+      { src:ImageData[1].src, caption:ImageData[1].caption },
+      { src:ImageData[2].src, caption:ImageData[2].caption },
+      { src:ImageData[3].src, caption:ImageData[3].caption },
+      { src:ImageData[4].src, caption:ImageData[4].caption },
+      { src:ImageData[5].src, caption:ImageData[5].caption },
+      { src:ImageData[6].src, caption:ImageData[6].caption },
+      { src:ImageData[7].src, caption:ImageData[7].caption }
     ]
   );
 
@@ -36,167 +31,35 @@ function App() {
     }
   }
 
-  function shuffle() {
-    setElements(elements.sort(()=> Math.random()- 0.5));
-  };
-
-  shuffle();
-
-  function pic1() {
-    if (pic1click === 0) {
-      setPic1(pic1click + 1);
+  const handleClick = (index) => {
+    if ( picClick[index] === 0) {
+      const newPic = [...picClick]
+      newPic[index] = 1;
+      setPicClick(newPic);
       setCount(count + 1);
       highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
     }
-  }
-  
-  function pic2() {
-    if (pic2click === 0) {
-      setPic2(pic2click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
+    else {
+      setPicClick(elements.map(() => 0))
       setCount(0);
     }
   }
 
-  function pic3() {
-    if (pic3click === 0) {
-      setPic3(pic3click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
-  }
-
-  function pic4() {
-    if (pic4click === 0) {
-      setPic4(pic4click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
-  }
-
-  function pic5() {
-    if (pic5click === 0) {
-      setPic5(pic5click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
-  }
-
-  function pic6() {
-    if (pic6click === 0) {
-      setPic6(pic6click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
-  }
-
-  function pic7() {
-    if (pic7click === 0) {
-      setPic7(pic7click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
-  }
-
-  function pic8() {
-    if (pic8click === 0) {
-      setPic8(pic8click + 1);
-      setCount(count + 1);
-      highScore();
-    } else {
-      setPic1(0);
-      setPic2(0);
-      setPic3(0);
-      setPic4(0);
-      setPic5(0);
-      setPic6(0);
-      setPic7(0);
-      setPic8(0);
-      setCount(0);
-    }
+  const handleElements = (number) => {
+    const newImageData = ImageData.slice(0,number);
+    setElements(newImageData);
   }
 
   return (
     <div className='app'>
       <div className='header'><Header /></div>
+      <div>Number of Pictures:<button className='selector' onClick={() => handleElements(4)}>4</button><button className='selector' onClick={() => handleElements(8)}>8</button><button className='selector' onClick={() => handleElements(12)}>12</button></div>
       <div className='score'><Score score={count} bestScore={bestScore} /></div>
       <div className="display">
-        {elements.map((index) => 
-          <div className='card' onClick={elements[index].function}><Card src={elements[index].src} caption={elements[index].caption} /></div>
-        )}
+        {elements.map((value, index) => 
+          <div key={index} className='card' onClick={() => handleClick(index)}><Card src={value.src} caption={value.caption} /></div>
+        ).sort(() => Math.random() - 0.5)}
       </div>
     </div>
   );
 }
-
-export default App;
